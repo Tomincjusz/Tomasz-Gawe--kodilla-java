b2570382ceac39c92a0cc2f1d551bf7bf205d462
Tomasz Gawe <tomincjusz@gmail.com>
2021-12-19 19:38:53 +0100

    Kodilla 6.2

--- kodilla testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,35 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
-    public static void main(String[] args){
-        System.out.println("Moduł 6 wprowadzenie do testowania oprogramowania");
+
+    public static void main(String[] args) {
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        Calculator simpleCalculator = new Calculator();
+        int result1 = simpleCalculator.addAToB(5, 5);
+        int result2 = simpleCalculator.subtractAFromB(10,5);
+        if(result1 == 10) {
+            System.out.println("test 1 is ok");
+        }
+        else {
+            System.out.println("Error");
+        }
+        if(result2 == 5) {
+            System.out.println("test 2 is ok");
+        }
+        else {
+            System.out.println("Error");
+        }
     }
 }
