b0458cdd30aac10c9303be4581c6938972ea84dc
Tomasz Gawe <tomincjusz@gmail.com>
2022-01-18 17:58:54 +0100

    Kodilla 7.2

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/immutable/ForumUser.java
@@ -0,0 +1,20 @@
+package com.kodilla.stream.immutable;
+
+public class ForumUser {
+
+    private String userName;
+    private String realName;
+
+    public ForumUser(final String userName, final String realName){
+        this.userName = userName;
+        this.realName = realName;
+    }
+
+    public String getUserName(){
+        return userName;
+    }
+
+    public String getRealName(){
+        return realName;
+    }
+}
